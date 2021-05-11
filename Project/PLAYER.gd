e07269a61f675528813aab3972777d3a4b4461f3
namiplusplus <namisocial@protonmail.com>
2021-05-11 18:27:39 -0500

    organized code + addded sum notes

--- Project/PLAYER.gd
+++ Project/PLAYER.gd
@@ -13,25 +13,22 @@ var dirX = 1
 var dirY = 1
 var jumpTime = 0
 
+var REALITY = 0
+
 #Constants
 const ACCELERATION = 0.80
 const FRICTION = 0.8
 const GRAVITY = 0.35
 const TOPXSPEED = 8
 
-#Realities
-enum MODE {RELAXED, PULSE}
-var REALITY = 0
-
 #Character Exclusive
 enum ROSTER {CHENDA, JESS ,NATAN, MELGAS}
 var CHARA = ROSTER.JESS
 
-var jessTeleLength = 0
+var JESSTELELENGTH = 0
 
-#TODO: port gms code to here
-var TIME = 359940
-	
+var TIME = 7200 #359940 IS ABSOLUTE MAXIMUM
+
 func _physics_process(_delta):
 #////////////////////////////////////Movement Setup/////////////////////////////////////
 	var move_vec = Vector3()
@@ -43,8 +40,6 @@ func _physics_process(_delta):
 	if moveY != 0: dirY = sign(moveY)
 	
 	hsp = clamp(hsp,-TOPXSPEED,TOPXSPEED)
-#////////////////////////////////////Movement Setup/////////////////////////////////////
-	
 #/////////////////////////////////Reality Switching Code////////////////////////////////
 	if Input.is_action_just_pressed("modeswitch"): 
 		REALITY += 1
@@ -55,26 +50,16 @@ func _physics_process(_delta):
 	set_collision_mask_bit(1,!REALITY)
 	set_collision_mask_bit(2,REALITY)
 	
-	if get_node_or_null("/root/Spatial/RELAXED"):
-		get_node("/root/Spatial/RELAXED").call("show" if !REALITY else "hide")
-	if get_node_or_null("/root/Spatial/PULSE"):
-		get_node("/root/Spatial/PULSE").call("show" if REALITY else "hide")
+	get_node("/root/Spatial/RELAXED").call("show" if !REALITY else "hide")
+	get_node("/root/Spatial/PULSE").call("show" if REALITY else "hide")
 	
 	get_node("REALITY_HUD").call("show" if !REALITY else "hide")
 	get_node("PULSE_HUD").call("show" if REALITY else "hide")
-#/////////////////////////////////Reality Switching Code////////////////////////////////
-	
-	
-	if get_node("cast").is_colliding():
-		var n = get_node("cast").get_collision_normal()
-		var slope_angle = (rad2deg(acos(n.dot(Vector3(0,-1,0)))) -180)*-1
-		
+#///////////////////////////////////Time Management Code//////////////////////////////////
 	print(str(TIME/3600).pad_zeros(2),":",str((TIME/60)%60).pad_zeros(2),":",str((TIME)%60).pad_zeros(2))
 	if Input.is_action_just_pressed("debug3"): TIME += 600
 	TIME -= 1
-	
-	
-	
+#/////////////////////////////////Horizontal Movement Code////////////////////////////////
 	match abs(moveX):
 		float(0): 
 			if abs(hsp) > 0: 
@@ -83,20 +68,7 @@ func _physics_process(_delta):
 				hsp = 0
 		float(1):
 			if abs(hsp) >= 0: hsp += ACCELERATION*dirX
-	
-	match CHARA:
-		ROSTER.JESS:
-			if Input.is_action_pressed("action2"): 
-				if jessTeleLength > 0:
-					hsp = moveX*20
-					vsp = moveY*20
-					jessTeleLength -= 0.1
-			else: jessTeleLength = 2
-		ROSTER.MELGAS:
-			pass #write gun code
-		ROSTER.NATAN:
-			pass #write bat swing code. you get the drill lol
-
+#/////////////////////////////////Vertical Movement Code////////////////////////////////
 	if !is_on_floor():
 		vsp -= GRAVITY
 		if !is_on_ceiling():
@@ -112,9 +84,30 @@ func _physics_process(_delta):
 			vsp = 6
 	else:
 		vsp = 0
-		
+	#This code is weirdly structured. Is there a way to make this better?
+#//////////////////////////////////////Character Code/////////////////////////////////////
+	match CHARA:
+		ROSTER.CHENDA:
+			pass
+		ROSTER.JESS:
+			if Input.is_action_pressed("action2"): 
+				if JESSTELELENGTH > 0:
+					hsp = moveX*20
+					vsp = moveY*20
+					JESSTELELENGTH -= 0.1
+			else: JESSTELELENGTH = 2
+		ROSTER.MELGAS:
+			pass #write gun code
+		ROSTER.NATAN:
+			pass #write bat swing code. you get the drill lol
+#//////////////////////////////////////Miscallaneous Code/////////////////////////////////////
 	move_vec.x = hsp
 	move_vec.y = vsp
 	move_vec.z = (1 if Input.is_action_pressed("debug1") else 0) - (1 if Input.is_action_pressed("debug2") else 0)*2;
-# warning-ignore:return_value_discarded
+	
+	#plcholder code for switching character angle along Z axis
+	if get_node("cast").is_colliding():
+		var n = get_node("cast").get_collision_normal()
+		var slope_angle = (rad2deg(acos(n.dot(Vector3(0,-1,0)))) -180)*-1
+	
 	move_and_slide(move_vec, Vector3(0,1,0))
