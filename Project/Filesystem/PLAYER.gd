2ae9d791c2835c22ca312a85f6bb3982f91360a3
namiplusplus <79037120+namiplusplus@users.noreply.github.com>
2021-05-29 13:04:34 -0500

    i did the
    yeah

--- Project/Filesystem/PLAYER.gd
+++ Project/Filesystem/PLAYER.gd
@@ -22,20 +22,19 @@ const FRICTION = 0.8
 const GRAVITY = 0.35
 const TOPXSPEED = 8
 
+#Modifiers
+var grvintensity = 1
+
 #Character Exclusive
 enum ROSTER {CHENDA, JESS ,NATAN, MELGAS}
 var CHARA = ROSTER.JESS
 
 var JESSTELELENGTH = 0
 
+
 var TIME = 3600 #359940 IS ABSOLUTE MAXIMUM
 
-func get_triangle_normal(a, b, c):
-	var side1 = b - a
-	var side2 = c - a
-	var normal = side1.cross(side2)
-	return normal
-	
+
 func _physics_process(_delta):
 #////////////////////////////////////Movement Setup/////////////////////////////////////
 	var move_vec = Vector3()
@@ -47,7 +46,41 @@ func _physics_process(_delta):
 	if moveY != 0: dirY = sign(moveY)
 	
 	hsp = clamp(hsp,-TOPXSPEED,TOPXSPEED)
+
+#//////////////////////////////////////Character Code/////////////////////////////////////
+	match REALITY:
+		0:
+			match CHARA:
+				ROSTER.CHENDA:
+					pass
+				ROSTER.JESS:
+					if Input.is_action_pressed("action2"):
+						if JESSTELELENGTH > 0:
+							hsp = moveX*20
+							vsp = moveY*20
+							JESSTELELENGTH -= 0.1
+					else: JESSTELELENGTH = 2
+				ROSTER.MELGAS:
+					pass #write gun code
+				ROSTER.NATAN:
+					pass #write bat swing code. you get the drill lol
+		1:
+			match CHARA:
+				ROSTER.CHENDA:
+					pass
+				ROSTER.JESS:
+					if Input.is_action_pressed("action2"):
+						if JESSTELELENGTH > 0:
+							hsp = 0
+							vsp = 0
+							JESSTELELENGTH = 0
+				ROSTER.MELGAS:
+					pass #write gun code
+				ROSTER.NATAN:
+					pass #write bat swing code. you get the drill lol
+	
 #/////////////////////////////////Reality Switching Code////////////////////////////////
+	
 	if Input.is_action_just_pressed("modeswitch"): 
 		REALITY += 1
 		get_node("FADE").show()
@@ -63,6 +96,7 @@ func _physics_process(_delta):
 	
 	get_node("REALITY_HUD").call("show" if !REALITY else "hide")
 	get_node("PULSE_HUD").call("show" if REALITY else "hide")
+
 #///////////////////////////////////Time Management Code//////////////////////////////////
 	#print(str(TIME/3600).pad_zeros(2),":",str((TIME/60)%60).pad_zeros(2),":",str((TIME)%60).pad_zeros(2))
 	if Input.is_action_just_pressed("debug3"): TIME += 600
@@ -78,42 +112,26 @@ func _physics_process(_delta):
 			if abs(hsp) >= 0: hsp += ACCELERATION*dirX
 #/////////////////////////////////Vertical Movement Code////////////////////////////////
 	if !is_on_floor():
-		vsp -= GRAVITY
+		vsp -= GRAVITY*grvintensity
 		if !is_on_ceiling():
-			if Input.is_action_pressed("action1"): 
+			if Input.is_action_pressed("action1") and (jumpTime != 20):
 				# TODO: make this feel less like crap
-				if jumpTime < 30: jumpTime += 1
-				if (jumpTime < 30): vsp = 9
-			else: jumpTime = 30
+				if jumpTime < 20: jumpTime += 1
+				if (jumpTime < 20): vsp = 12
+			else: 
+				grvintensity = 1.5
+				jumpTime = 20
 	elif !is_on_ceiling():
 		vsp = 0
 		jumpTime = 0
-		if Input.is_action_just_pressed("action1"):
-			#play sound
-			vsp = 6
 	else:
 		vsp = 0
 	#This code is weirdly structured. Is there a way to make this better?
-#//////////////////////////////////////Character Code/////////////////////////////////////
-	match CHARA:
-		ROSTER.CHENDA:
-			pass
-		ROSTER.JESS:
-			if Input.is_action_pressed("action2"): 
-				if JESSTELELENGTH > 0:
-					hsp = moveX*20
-					vsp = moveY*20
-					JESSTELELENGTH -= 0.1
-			else: JESSTELELENGTH = 2
-		ROSTER.MELGAS:
-			pass #write gun code
-		ROSTER.NATAN:
-			pass #write bat swing code. you get the drill lol
-		
+
 	degreetest += (1 if Input.is_action_pressed("debug3") else 0) - (1 if Input.is_action_pressed("debug4") else 0)
-	
-	
+		
 #//////////////////////////////////////Miscallaneous Code/////////////////////////////////////
+
 	move_vec.x = hsp
 	move_vec.y = vsp
 	move_vec.z = (1 if Input.is_action_pressed("debug9") else 0) - (1 if Input.is_action_pressed("debug0") else 0)*2;
--- Project/Filesystem/TITLE.gd
+++ Project/Filesystem/TITLE.gd
@@ -1,11 +1,15 @@
 extends Node
 var begin = false
+var turn = 0
+
 
 func _process(delta):
+	turn += 1
 	if Input.is_action_just_pressed("start") && begin == false:
-		get_node("AnimationPlayer").seek(5)
+		get_node("INTRO").seek(5)
 		get_node("/root/GLOBAL/DRAW_TOP_PRIORITY").scene_change("res://Filesystem/STAGE/STAGE1.tscn",2)
 		begin = true
 	if (begin == true):
-		get_node("AnimationPlayer2").play("SELECT")
-	else: get_node("AnimationPlayer2").play("FLICKER")
+		get_node("TEXT_FLASH").play("SELECT")
+	else: get_node("TEXT_FLASH").play("FLICKER")
+	get_node("CAM_SPIN").play("SPIN")
